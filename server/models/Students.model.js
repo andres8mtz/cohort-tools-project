166f46234b58b1f9e3e7e6c7b56c9ea5e749a0b1
Andres Ochoa <andres8mtz@gmail.com>
2025-03-01 14:52:10 +0100

    created database

---
+++ server/models/Students.model.js
@@ -0,0 +1,34 @@
+// ./models/Cohorts.model.js
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+// CREATE SCHEMA
+// Schema - describes and enforces the structure of the documents
+const studentSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    phone: String,
+    linkedinUrl: String,
+    languages: [{
+        type: String,
+        enum: ["English", "Dutch", "Spanish", "French", "German", "Portuguese"],
+    }],
+    program: String,
+    background: String,
+    image: String,
+    cohort: String,
+    projects: [String]
+});
+
+
+// CREATE MODEL
+// The model() method defines a model (Student) and creates a collection (books) in MongoDB
+// The collection name will default to the lowercased, plural form of the model name:
+//                          "Student" --> students
+const Student = mongoose.model("Student", studentSchema);
+
+
+// EXPORT THE MODEL
+module.exports = Student;
--- server/seeds/students.seed.js
+++ server/seeds/students.seed.js
@@ -2,24 +2,24 @@
 require('dotenv').config()
 const mongoose = require("mongoose");
 
-const Cohort = require("../models/Cohorts.model");
+const Student = require("../models/Students.model");
 
-const cohorts = require("../cohorts.json");
+const students = require("../students.json");
 
   // ... your code here
   mongoose
   .connect(process.env.MONGO_URI)
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .then(() => {
-    Cohort.create(cohorts)
-      .then((cohorts) => {
-        console.log("Cohorts created ->", cohorts);
+    Student.create(students)
+      .then((students) => {
+        console.log("Students created ->", students);
         mongoose.connection.close();
       })
       .catch((error) => {
-        console.error("Error while creating cohorts ->", error);
+        console.error("Error while creating students ->", error);
         mongoose.connection.close();
       });
   })
 
-module.exports = cohorts;
+module.exports = students;
