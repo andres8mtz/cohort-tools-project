372f59d2d78b190801c90568c8972dc4b3c8707c
theresajvrensburg <theresa@isogramconsulting.com>
2025-03-11 18:33:56 +0100

    authenticated

--- server/app.js
+++ server/app.js
@@ -21,7 +21,8 @@ mongoose
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
-
+const { isAuthenticated } = require("../server/middleware");
+const User = require("../server/models/User.model");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
@@ -38,6 +39,14 @@ app.use(cookieParser());
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
+
+app.get("/api/users/:id", isAuthenticated, (req, res, next) => {
+  console.log("req.params.id", req.params.id);
+  User.findById(req.params.id).then((User) => 
+    res.json(User)).catch((err) => {
+    res.status(500).json({ message: "Error while getting user details" });
+  });
+})
 
 const studentsRoutes = require("./routes/student.routes");
 app.use("/", studentsRoutes);
